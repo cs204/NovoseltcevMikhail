637de20e315f0401669a5375a5d6392869bbf9cb
test <test>
2023-10-08 20:56:57 +0700

    test

---
+++ extensions.py
@@ -0,0 +1,15 @@
+x =  (input("File name: "))
+if x== ("hello.jpg"):
+    print("image/jpeg")
+if x== ("document.pdf"):
+    print("application/pdf")
+if x== (".gif"):
+    print("image/gif")
+if x== (".jpeg"):
+    print("image/jpeg")
+if x== (".png"):
+    print("image/png")
+if x== (".txt"):
+    print("text/plain")
+if x== (".zip"):
+    print("application/zip")
